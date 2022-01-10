61554d736bd81c08666c451e57272c6f5b319c47
Vineet Verma <vermavineet87@gmail.com>
2022-01-11 01:10:56 +0530

    Initial commit

---
+++ imageRead.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import os
+
+
+def readimage():
+    img = cv2.imread('Images/bostondynamics.jpg', cv2.IMREAD_COLOR)
+
+    #cv2.imshow('IMAGE', img)
+    return img
+
+
+def reduceimageBy2(img):
+    img = img
+    height, width, channels = img.shape
+    print(height, width)
+    for i in range(0, height):
+        for j in range(0, width):
+            #img[i, j] = img[i, j]-2
+            (b, g, r) = img[i, j]
+            #print((b, g, r))
+            b= b/2
+            g = g/2
+            r = r/2
+            img[i, j] = (b, g, r)
+            #(b, g, r) = img[i, j]
+            #print((b, g, r))
+    #filename = 'savedImage.jpg'
+    directory = r'C:\Users\Nagraj\PycharmProjects\ComputerVision01\Images'
+    os.chdir(directory)
+    cv2.imwrite('savedImage.jpg', img)
+
+def imageresize():
+    img = cv2.imread('Images/bostondynamics.jpg', cv2.IMREAD_COLOR)
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    width = 600
+    height = 800
+    dim = (width, height)
+    resized = cv2.resize(gray_image, dim, interpolation=cv2.INTER_AREA)
+    directory = r'C:\Users\Nagraj\PycharmProjects\ComputerVision01\Images'
+    os.chdir(directory)
+    cv2.imwrite('grayscaleImageresized.jpg', resized)
+    cv2.imshow("Resized image", resized)
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
+
+import imageRead
+import os
+
+
+if __name__ == '__main__':
+
+    img = imageRead.readimage()
+    print(os.listdir())
+    #imageRead.reduceimageBy2(img)
+    imageRead.imageresize()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
